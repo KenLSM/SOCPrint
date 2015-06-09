4f9cb8102a769272643659b6d43830c23d1824ee
KenLSM <kenlsm@u.nus.edu>
2015-06-09 16:28:13 +0800

    Changed checklogin to JSON Changed index to AJAX and JSON based for login function. Added comments to logic.

--- html/checklogin.php
+++ html/checklogin.php
@@ -1,50 +1,33 @@
-<!--
-PHP Page used for checking login credentials
-Last update 5 June 2015
-
-Have not removed ssh2 implementation. Might want to remove.
-String texts are fixed. Has to be similar to index.html's js implementation.
--->
 <?php
+	/*
+	PHP Page used for checking login credentials
+	Last update 8 June 2015
 
+	String texts are fixed. Has to be similar to index.html's js implementation.
+	*/
 	$serverAddr = "sunfire-r.comp.nus.edu.sg";
 	$serverPort = 22;
-	$userName =  $_POST["name"];
-	$userPass = $_POST["password"];
+	
+	$data = json_decode(file_get_contents('php://input'), true);
+	$userName =  $data["name"];
+	$userPass = $data["password"];
 	
 	// NET_SS2 Implementation
-	
+	$message = "No message";
 	include('Net/SSH2.php');
 	$ssh = new Net_SSH2($serverAddr);
 	if(!$ssh)
 	{
-		echo "NO_CONNECTION";
+		$message = "NO_CONNECTION";
 	}
-	if (!$ssh->login($userName, $userPass))
+	if (!$ssh->login($userName , $userPass))
 	{
-		echo "WRONG";
+		$message = "WRONG";
 	}
 	else
 	{
-		echo "OK";
+		$message = "OK";
 	}
-	
-	/*
-	// SSH2 Implementation
-	if(!($con = ssh2_connect($serverAddr, $serverPort)))
-	{
-		echo "NO_CONNECTION";
-	} else 
-	{
-		// try to authenticate with username root, password secretpassword
-		if(!ssh2_auth_password($con, $userName, $userPass)) 
-		{
-			echo "WRONG";
-		} else 
-		{
-			// allright, we're in!
-			echo "OK";
-		}
-	}
-	*/
+	echo json_encode($message);
+
 ?>
