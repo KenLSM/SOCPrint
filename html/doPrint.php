1070f74f69c0abf126352e46c6a1ab42877f81aa
KenLSM <kenlsm@u.nus.edu>
2015-08-03 15:20:12 +0800

    Fixed printer assignment and deletion of temporary files created

--- html/doPrint.php
+++ html/doPrint.php
@@ -5,13 +5,17 @@
 	$serverAddr = "sunfire-r.comp.nus.edu.sg";
 	$serverPort = 22;	
 
+	$deleteCommand = "find ~/socPrint/ -type f -name 'SP_*' -exec rm {} \;";
+	
 	$data = json_decode(file_get_contents('php://input'), true);
 	$userName =  $_POST["username"];
 	$userPass = $_POST["password"];
 	$fileName = $_POST["fileName"];
+	$printer = $_POST["printer"];
 	$message = "";
 	$verbose = "";
 	$errorMSG = "";
+	$fileName = substr($fileName, 0, -4);
 	$ssh = new Net_SSH2($serverAddr);
 	if(!$ssh)
 	{
@@ -23,8 +27,18 @@
 	}
 	else
 	{
+		if($printer === "psts" || $printer === "pstsb" || $printer === "pstsc")
+		{	
+			$message  = $ssh->exec("lpr -P $printer socPrint/$fileName.ps");
+		}
+		else
+		{
+			$message = "invalid printer provided: $printer";
+		}
 		// Command for execution in sunfire: pdftops [input file] [output file]
 		//$message  = $ssh->exec("lpr -P psts socPrint/$fileName.ps");
+		$ssh->exec($deleteCommand);
+		$ssh->exec("command rm socPrint/$fileName.ps");
 	}
 	
 	$array = array("status" => $message);
