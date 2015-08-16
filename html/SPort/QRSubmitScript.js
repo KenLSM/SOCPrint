87e7cd08b07e6a270e0e2618f4fe0a564fa2b135
KenLSM <kenlsm@u.nus.edu>
2015-08-16 14:58:42 +0800

    for sgport

---
+++ html/SPort/QRSubmitScript.js
@@ -0,0 +1,245 @@
+// QRCODE reader Copyright 2011 Lazar Laszlo
+// http://www.webqr.com
+
+var gCtx = null;
+var gCanvas = null;
+var c=0;
+var stype=0;
+var gUM=false;
+var webkit=false;
+var moz=false;
+var v=null;
+
+var imghtml='<div id="qrfile"><canvas id="out-canvas" width="320" height="240"></canvas>'+
+    '<div id="imghelp">drag and drop a QRCode here'+
+	'<br>or select a file'+
+	'<input type="file" onchange="handleFiles(this.files)"/>'+
+	'</div>'+
+'</div>';
+
+var vidhtml = '<video id="v" autoplay style="width:100%; height:50%"></video>';
+
+function dragenter(e) {
+  e.stopPropagation();
+  e.preventDefault();
+}
+
+function dragover(e) {
+  e.stopPropagation();
+  e.preventDefault();
+}
+function drop(e) {
+  e.stopPropagation();
+  e.preventDefault();
+
+  var dt = e.dataTransfer;
+  var files = dt.files;
+  if(files.length>0)
+  {
+	handleFiles(files);
+  }
+  else
+  if(dt.getData('URL'))
+  {
+	qrcode.decode(dt.getData('URL'));
+  }
+}
+
+function handleFiles(f)
+{
+	var o=[];
+	
+	for(var i =0;i<f.length;i++)
+	{
+        var reader = new FileReader();
+        reader.onload = (function(theFile) {
+        return function(e) {
+            gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height);
+
+			qrcode.decode(e.target.result);
+        };
+        })(f[i]);
+        reader.readAsDataURL(f[i]);	
+    }
+}
+
+function initCanvas(w,h)
+{
+    gCanvas = document.getElementById("qr-canvas");
+    gCanvas.style.width = w + "px";
+    gCanvas.style.height = h + "px";
+    gCanvas.width = w;
+    gCanvas.height = h;
+    gCtx = gCanvas.getContext("2d");
+    gCtx.clearRect(0, 0, w, h);
+}
+
+
+function captureToCanvas() {
+    if(stype!=1)
+        return;
+    if(gUM)
+    {
+        try{
+            gCtx.drawImage(v,0,0);
+            try{
+                qrcode.decode();
+            }
+            catch(e){       
+                console.log(e);
+                setTimeout(captureToCanvas, 500);
+            };
+        }
+        catch(e){       
+                console.log(e);
+                setTimeout(captureToCanvas, 500);
+        };
+    }
+}
+
+function htmlEntities(str) {
+    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
+
+function read(a)
+{
+    var html="<br>";
+
+	/*
+    // Open web if is some htmp
+    
+    if(a.indexOf("http://") === 0 || a.indexOf("https://") === 0)
+        html+="<a target='_blank' href='"+a+"'>"+a+"</a><br>";
+    
+    // Do some formatting of returned value
+    html+="<b>"+htmlEntities(a)+"</b><br><br>";
+    // Set to var
+    document.getElementById("result").innerHTML=html;
+    */
+    
+    // Do call back
+    
+    // REMOVE THIS AFTER IMPLEMENTING INTO GAE
+    //alert(a);
+    
+    //var path = "/verifyQRForCheckin";
+    //var method = "post";
+    
+    //var form = document.createElement("form");
+    //form.setAttribute("method", method);
+    //form.setAttribute("path", path);
+    //form.setAttribute("name", a);
+    
+    changeColour(true);
+    
+    //form.submit();
+    
+     document.getElementById("textmsg").innerHTML = "QR Code accepted. Please proceed to Express Queue.";
+     //alert(a);
+}	
+
+function isCanvasSupported(){
+  var elem = document.createElement('canvas');
+  return !!(elem.getContext && elem.getContext('2d'));
+}
+function success(stream) {
+    if(webkit)
+        v.src = window.webkitURL.createObjectURL(stream);
+    else
+    if(moz)
+    {
+        v.mozSrcObject = stream;
+        v.play();
+    }
+    else
+        v.src = stream;
+    gUM=true;
+    setTimeout(captureToCanvas, 500);
+}
+		
+function error(error) {
+    gUM=false;
+    return;
+}
+
+function load()
+{
+	//changeColour(true);
+	if(isCanvasSupported() && window.File && window.FileReader)
+	{
+		initCanvas(800, 600);
+		qrcode.callback = read;
+		document.getElementById("mainbody").style.display="block";
+        setwebcam();
+	}
+	else
+	{
+		document.getElementById("mainbody").style.display="inline";
+		document.getElementById("mainbody").innerHTML='<p id="mp1">QR code scanner for HTML5 capable browsers</p><br>'+
+        '<br><p id="mp2">sorry your browser is not supported</p><br><br>'+
+        '<p id="mp1">try <a href="http://www.mozilla.com/firefox"><img src="firefox.png"/></a> or <a href="http://chrome.google.com"><img src="chrome_logo.gif"/></a> or <a href="http://www.opera.com"><img src="Opera-logo.png"/></a></p>';
+	}
+}
+
+function setwebcam()
+{
+    if(stype==1)
+    {
+        setTimeout(captureToCanvas, 500);    
+        return;
+    }
+    var n=navigator;
+    document.getElementById("outdiv").innerHTML = vidhtml;
+    v=document.getElementById("v");
+
+    if(n.getUserMedia)
+        n.getUserMedia({video: true, audio: false}, success, error);
+    else
+    if(n.webkitGetUserMedia)
+    {
+        webkit=true;
+        n.webkitGetUserMedia({video:true, audio: false}, success, error);
+    }
+    else
+    if(n.mozGetUserMedia)
+    {
+        moz=true;
+        n.mozGetUserMedia({video: true, audio: false}, success, error);
+    }
+    
+
+    stype=1;
+    setTimeout(captureToCanvas, 500);
+}
+function setimg()
+{
+	document.getElementById("result").innerHTML="";
+    if(stype==2)
+        return;
+    document.getElementById("outdiv").innerHTML = imghtml;
+    
+}
+
+
+function remove(str)
+{
+		$("#mainbody").removeClass(str); 
+		$("#textmsg").addClass("hidden");
+		setTimeout(function(){location.reload();}, 1500);
+}
+
+function changeColour(bool)
+{
+	var div = $("#mainbody");
+	if(bool)
+	{
+		div.addClass("green");
+		setTimeout('remove("green")', 2000);
+		
+	}
+	else
+	{
+		div.addClass("red");
+		setTimeout('remove("red")', 2000);
+	}
+}
